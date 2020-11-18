cf064555df4c06cb0018f6c2083fe0e968705332
Richard <rrosenthal2013@gmail.com>
2020-11-18 17:38:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+newWeight = float(weight)
+newHeight = float(height)
+BMI = newWeight/(newHeight**newHeight)
+print("Your BMI is :" + str(BMI) + ". The more you know...")
+
+
+
+
+
+
+
+
+
